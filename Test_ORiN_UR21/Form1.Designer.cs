699b90b210b5163affa9d23947632c98843bba70
scsfdev <scsfdev@github>
2023-08-23 14:11:31 +0800

    A test demo software to get Uii UHF data from UR21 via ORiN2 SDK.

--- Test_ORiN_UR21/Form1.Designer.cs
+++ Test_ORiN_UR21/Form1.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtController = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCOM = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lstTags = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(82, 182);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(74, 42);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtController
+            // 
+            this.txtController.Location = new System.Drawing.Point(130, 63);
+            this.txtController.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtController.Name = "txtController";
+            this.txtController.Size = new System.Drawing.Size(172, 20);
+            this.txtController.TabIndex = 1;
+            this.txtController.Text = "UR21controller";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Controller Name:";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(170, 182);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(74, 42);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Provider Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.textBox2.Location = new System.Drawing.Point(130, 106);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "CaoProv.DENSO.UR20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "COM Port:";
+            // 
+            // txtCOM
+            // 
+            this.txtCOM.Location = new System.Drawing.Point(130, 148);
+            this.txtCOM.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCOM.Name = "txtCOM";
+            this.txtCOM.Size = new System.Drawing.Size(41, 20);
+            this.txtCOM.TabIndex = 6;
+            this.txtCOM.Text = "9";
+            this.txtCOM.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(82, 240);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(74, 42);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "TAG READ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lstTags
+            // 
+            this.lstTags.FormattingEnabled = true;
+            this.lstTags.Location = new System.Drawing.Point(339, 63);
+            this.lstTags.Name = "lstTags";
+            this.lstTags.Size = new System.Drawing.Size(287, 264);
+            this.lstTags.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 341);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Total:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(374, 341);
+            this.lblTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(10, 13);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 387);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstTags);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCOM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtController);
+            this.Controls.Add(this.btnConnect);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtController;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCOM;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.ListBox lstTags;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- Test_ORiN_UR21/Form1.cs
+++ Test_ORiN_UR21/Form1.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CAOLib;
 
 namespace Test_ORiN_UR21
 {
     public partial class Form1 : Form
     {
+        CaoEngine engine;
+        CaoWorkspace workspace;
+        CaoController controller;
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            engine = new CaoEngine();
+            workspace = engine.Workspaces.Item(0);
+
+            btnDisconnect.Enabled = btnRead.Enabled = false;
+            btnConnect.Enabled = true;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                controller = workspace.AddController(txtController.Text.Trim(), "CaoProv.DENSO.UR20", "", "conn=COM:" + txtCOM.Text.Trim());
+
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+
+                btnRead.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error");
+            }            
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                workspace.Controllers.Remove(controller.Index);
+                controller = null;
+
+                engine.Workspaces.Remove(workspace.Index);
+                workspace = null;
+
+                engine = null;
+
+                btnConnect.Enabled = true;
+
+                btnRead.Enabled = btnDisconnect.Enabled = false;
+
+                btnConnect.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }           
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            lstTags.Items.Clear();
+            readTags2();
+        }
+
+        private void readTags2()
+        {
+            try
+            {
+                object[] tags = controller.Execute("ReadUii", false);
+                if (tags != null && tags.Length > 0)
+                {
+                    Console.WriteLine("Tags Read: " + tags.Length);
+                    lblTotal.Text = tags.Length.ToString();
+
+                    for (int iTag = 0; iTag < tags.Length; iTag++)
+                    {
+                        // PC data.
+                        object[] tag = tags[iTag] as object[];
+                        Console.WriteLine("PC Data:" + tag[0].ToString());
+
+                        // Get UII Data
+                        byte[] uiiBytes = tag[1] as byte[];
+                        Console.WriteLine("Uii: " + BitConverter.ToString(uiiBytes).Replace("-", ""));
+
+                        lstTags.Items.Add(BitConverter.ToString(uiiBytes).Replace("-", ""));
+                    }
+                }
+                else
+                {
+                    lblTotal.Text = "-";
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void readTags1()
+        {
+            try
+            {
+                var tags = controller.Execute("ReadUii", false);
+                if(tags != null)
+                {
+                    long items = (tags as Array).GetUpperBound(0);
+                    for (long item = 0; item < items; item++)
+                    {
+                        // PC data.
+                        long pcData = tags[item][0];
+                        Console.WriteLine("PC Data:" + pcData.ToString());
+
+                        // Get UII Data
+                        byte[] uiiBytes = tags[item][1];
+
+                        // Uii Data Size.
+                        long uiiSize = (uiiBytes as Array).GetUpperBound(0);
+                        for (long j = 0; j < uiiSize; j++)
+                        {
+                            byte value = uiiBytes[j];
+                            Console.WriteLine("Byte: " + value);
+                        }
+
+                        Console.WriteLine(BitConverter.ToString(uiiBytes).Replace("-", ""));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
